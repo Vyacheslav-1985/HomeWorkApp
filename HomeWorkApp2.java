a54e4e650421fc81474215c402dadacbc6e97a25
Vyacheslav <maksimov-1985@yandex.ru>
2021-12-14 21:57:42 +0300

    Initial commit

---
+++ HomeWorkApp2.java
@@ -0,0 +1,41 @@
+/**
+ * Java 1. Homework 2
+ * @Author Maksimov Vyacheslav
+ * @version 15.12.2021
+ *
+ */
+class HomeWorkApp2 {
+    
+	public static void main(String[]args) {
+        
+	    System.out.println(Сhisla(10, 5));
+	    ChisloPozNeg(1);
+	    System.out.println(ChisloNegative(2));
+	    StrokaChislo("NewYear",6);
+	}   
+	
+	static boolean Сhisla(int a, int b) {
+        return (a + b >= 10 && a + b <= 20);
+    }
+ 
+        static void ChisloPozNeg(int a) {
+	    if (a >= 0) {
+        System.out.println("positive");
+    }   else {
+        System.out.println("negative");
+    }
+    }
+
+    static boolean ChisloNegative(int a) {
+        return (a < 0);
+    }
+    
+	static void StrokaChislo(String s, int Cycle) {
+        for (int i = 0; i < Cycle; i++) {
+        System.out.println(s);
+    }
+    }   
+
+
+
+} 
